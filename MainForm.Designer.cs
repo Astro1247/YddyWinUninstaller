2a8e9ff701772fc85dac9ad887ac8f8b69559744
Astro <taynik777@gmail.com>
2019-05-25 10:32:28 +0300

    V1.2

---
+++ MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace YddyWinUninstaller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.centerlabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(990, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добро пожаловать в универсальный деинсталятор WINDOWS эксклюзивно для YDDY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.label2.Location = new System.Drawing.Point(688, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пути назад нет, поехали!";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Calibri", 50F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.button1.Location = new System.Drawing.Point(110, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(780, 462);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "СЛОМАТЬ ВИНДУ!\r\nЖМИ СЮДА!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 635);
+            this.progressBar1.MarqueeAnimationSpeed = 220;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(976, 43);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // centerlabel
+            // 
+            this.centerlabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.centerlabel.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.centerlabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.centerlabel.Location = new System.Drawing.Point(0, 635);
+            this.centerlabel.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.centerlabel.Name = "centerlabel";
+            this.centerlabel.Size = new System.Drawing.Size(1000, 55);
+            this.centerlabel.TabIndex = 4;
+            this.centerlabel.Text = "1";
+            this.centerlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.centerlabel.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.BurlyWood;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(905, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ВЫХОД";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.BackgroundImage = global::YddyWinUninstaller.Properties.Resources.yddyrp_sign;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1000, 690);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.centerlabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "YDDY:Windows Uninstaller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label centerlabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace YddyWinUninstaller
+{
+    public enum MonitorState
+    {
+        MonitorStateOn = -1,
+        MonitorStateOff = 2,
+        MonitorStateStandBy = 1
+    }
+    public partial class MainForm : Form, IMessageFilter
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int WM_LBUTTONDOWN = 0x0201;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private HashSet<Control> controlsToMove = new HashSet<Control>();
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_LBUTTONDOWN &&
+                controlsToMove.Contains(Control.FromHandle(m.HWnd)))
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                return true;
+            }
+            return false;
+        }
+
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //Application.AddMessageFilter(this);
+
+            //controlsToMove.Add(this);
+            
+            //controlsToMove.Add(this.label1);
+            //controlsToMove.Add(this.label2);
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+
+
+        }
+
+        void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            SetMonitorInState(MonitorState.MonitorStateOff);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            this.button1.Enabled = false;
+            say("О, нет..\nЧто же ты наделал..\nТы слышишь это?...\nЧто-то происходит...", 4500);
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            say("Куда все делось?\nЧто происходит?...\nМне страшно :(", 6500);
+            
+            this.WindowState = FormWindowState.Maximized;
+            this.progressBar1.Visible = true;
+            button1.Scale(1, 0.3f);
+            for (int i = 0; i < 100; i++)
+            {
+                switchButton();
+                this.BackColor = Color.Blue;
+                button1.BackColor = Color.Red;
+                delay(rnd.Next(3,400));
+                switchButton();
+                this.BackColor = Color.Red;
+                button1.BackColor = Color.Blue;
+                delay(rnd.Next(3, 400));
+                this.progressBar1.Value = i;
+                switchButton();
+            }
+            this.button1.Visible = false;
+            this.button1.Update();
+            this.BackColor = Color.FromArgb(33, 33, 33);
+            this.progressBar1.Visible = false;
+            //this.WindowState = FormWindowState.Normal;
+            controlsToMove.Add(this);
+            delay(5000);
+
+            centerlabel.Visible = true;
+            sayc("Хм, кажется это закончилось?", 2000);
+            sayc("", 4000);
+            sayc("Интересно, а WINDOWS еще работает?...", 3000);
+            sayc("", 4000);
+            sayc("О, нет...", 2000);
+            sayc("Я просто кое-что вижу...", 3000);
+            sayc("Ты пока не видишь, да?...", 4000);
+            sayc("Тут кое-что приближается..", 4000);
+            sayc("Прости...", 2500);
+            sayc("", 4000);
+            for (int i = 0; i < 35; i++)
+            {
+                this.BackColor = Color.FromArgb(33, 33, 33);
+                delay(rnd.Next(1, 250));
+                this.BackColor = Color.FromArgb(255, 215, 50);
+                delay(rnd.Next(1, 250));
+                this.BackColor = Color.FromArgb(250, 250, 250);
+                delay(rnd.Next(1, 250));
+                this.BackColor = Color.FromArgb(253, 167, 59);
+                delay(rnd.Next(1, 250));
+            }
+
+            this.BackColor = Color.FromArgb(33, 33, 33);
+            sayc("", 10000);
+            sayc("Эмм... Ты еще тут??", 4000);
+            sayc("", 4000);
+            sayc("Странно..", 4000);
+            sayc("", 4000);
+            sayc("А, вижу, вот теперь мне становится страшно...", 4000);
+            sayc("", 4000);
+            sayc("Прощай", 2000);
+            sayc("", 4000);
+            sayc("А, еще кое что, помни, движение мышки тебе поможет!", 4000);
+            sayc("", 4000);
+
+            for (int i = 0; i < 150; i++)
+            {
+                SetMonitorInState(MonitorState.MonitorStateOff);
+                Thread.Sleep(50);
+            }
+            sayc("", 10000);
+            sayc("Фух, еле пронесло, а ты там как?", 5000);
+            sayc("Пора бы заканчивать эти шутки...", 4000);
+            sayc("Я поищу кнопку...", 4000);
+            sayc("", 6000);
+            sayc("Кажется нашел, сейчас попробую показать ее", 5000);
+            sayc("", 2000);
+            sayc("Видишь кнопку выхода?", 2500);
+            sayc("Как нет?..", 2000);
+            sayc("Блин, погоди...", 2500);
+            sayc("", 4000);
+            button2.Visible = true;
+            button2.Update();
+            sayc("Жми быстрее!!!", 2000);
+            sayc("", 4000);
+            sayc("Эм, ты почему не вышел?", 4000);
+            sayc("", 4000);
+            sayc("В смысле не нажимается?...", 2500);
+            sayc("Секунду...", 2000);
+            sayc("", 4000);
+            button2.Enabled = true;
+            button2.Update();
+            sayc("Пробуй", 4000);
+        }
+
+        public int state = 1;
+
+        public void switchButton()
+        {
+            switch (state)
+            {
+                case 1:
+                    say("Удаляем WINDOWS.");
+                    state++;
+                    break;
+                case 2:
+                    say("Удаляем WINDOWS. .");
+                    state++;
+                    break;
+                case 3:
+                    say("Удаляем WINDOWS. . .");
+                    state++;
+                    break;
+                case 4:
+                    say("Удаляем WINDOWS. . . .");
+                    state++;
+                    break;
+                case 5:
+                    say("Удаляем WINDOWS. . .");
+                    state++;
+                    break;
+                case 6:
+                    say("Удаляем WINDOWS. .");
+                    state++;
+                    break;
+                case 7:
+                    say("Удаляем WINDOWS.");
+                    state++;
+                    break;
+                case 8:
+                    say(".Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 9:
+                    say(". .Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 10:
+                    say(". . .Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 11:
+                    say(". . . .Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 12:
+                    say(". . .Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 13:
+                    say(". .Удаляем WINDOWS");
+                    state++;
+                    break;
+                case 14:
+                    say(".Удаляем WINDOWS");
+                    state = 1;
+                    break;
+                default:
+                    state = 1;
+                    break;
+            }
+        }
+
+        public void say(string text, int delay = 0)
+        {
+            this.button1.Text = text;
+            this.button1.Update();
+            if (delay != 0) MainForm.delay(delay);
+        }
+
+        public void sayc(string text, int delay = 0)
+        {
+            centerlabel.Text = text;
+            centerlabel.Update();
+            if(delay != 0) MainForm.delay(delay);
+        }
+
+        public void wait(int msec)
+        {
+            Thread.Sleep(msec);
+        }
+
+
+
+        public static void delay(int seconds)
+        {
+            if (seconds < 1) return;
+            DateTime _desired = DateTime.Now.AddMilliseconds(seconds);
+            while (DateTime.Now < _desired)
+            {
+                Thread.Sleep(1);
+                System.Windows.Forms.Application.DoEvents();
+            }
+        }
+
+
+
+        private void SetMonitorInState(MonitorState state)
+        {
+            SendMessage(0xFFFF, 0x112, 0xF170, (int)state);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
